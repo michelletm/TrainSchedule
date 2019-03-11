87c8d86f4e4c99c88606b916ba31843c988f2b28
michelletm <michellemirek@gmail.com>
2019-03-10 23:43:47 -0700

    final js touches

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TrainSchedule
+
+This project uses jQuery and moment.js to create a dynamic table that can be appended with new information, and stay current on times and related calculations. 
--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
-var currentTime = moment();
-console.log(moment(currentTime).format("hh:mm"));
+var currentTime = moment().format();
+console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm a"));
 
 function addTrain () {
 
@@ -8,13 +8,10 @@ function addTrain () {
    var firsttrain = $("#firstTrainTime").val().trim();
    var frequency =  $("#frequency").val().trim();
    
-
 }
 
 
 $(document).ready(function() {
-
-
 
    $("#submit-btn").on("click", function() {
       
@@ -32,7 +29,22 @@ $(document).ready(function() {
       $("#trainDestination").val("");
       $("#firstTrainTime").val("");
       $("#frequency").val("");
-         
+
+      var firstTimeConverted = moment(firsttrain, "hh:mm a").subtract(1, "years");
+      console.log("First Time Converted: " + firstTimeConverted);
+
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      console.log("Difference in Time: " + diffTime);
+   
+      var tRemainder = diffTime % frequency;
+      console.log(tRemainder);
+
+      var nextarrival = moment().add(minaway, "minutes");
+      console.log("Next Arrival: " + moment(nextarrival).format("hh:mm a"));
+
+      var minaway = frequency - tRemainder;
+      console.log("Minutes Until Next Train: " + minaway);
+
          
       $(".table").find("tbody")
          .append($("<tr>")
@@ -40,21 +52,14 @@ $(document).ready(function() {
                .text(trainname))
             .append($("<td>")
                .text(destination))
-            // .append($("<td>")
-            //    .text(firsttrain))
             .append($("<td>")
-               .text(frequency)
-            )
+               .text(frequency))
+            .append($("<td>")
+               .text((nextarrival).format("hh:mm a")))
+            .append($("<td>")
+               .text(minaway))
          )
-
-   
-   
-   
       
-      
-      
-      
-     
    });
 })     
      
